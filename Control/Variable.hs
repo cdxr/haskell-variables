33761d42ae06e9e3a2036b9dfff646fd67c48285
Craig Roche <cdxr01@gmail.com>
2014-08-04 10:22:44 -0400

    added documentation for most of the API

--- Control/Variable.hs
+++ Control/Variable.hs
@@ -22,18 +22,23 @@ import Control.Concurrent.STM
 -- | The class @Variable m v@ defines operations for reading and writing to
 -- a variable of type @v@ in the monad @m@.
 class (Monad m) => Variable m v | v -> m where
+    -- | @writeVar v s@ stores the value @s@ in @v@.
     writeVar :: v s -> s -> m ()
     writeVar v s = modifyVar v $ const ((), s)
 
+    -- | @readVar v@ retrieves the value stored in @v@.
     readVar :: v s -> m s
     readVar v = modifyVar v $ \s -> (s, s)
 
+    -- | @modifyVar v f@ applies @f@ to the value stored in @v@, storing
+    -- the new value in @v@.
     modifyVar  :: v s -> (s -> (a, s)) -> m a
     modifyVar v f = do
         (a, s) <- f `liftM` readVar v
         writeVar v s
         return a
 
+    -- | A version of `modifyVar` that applies the function strictly.
     modifyVar' :: v s -> (s -> (a, s)) -> m a
     modifyVar' v f = modifyVar v $ \s0 ->
         let (a, s) = f s0
@@ -106,24 +111,38 @@ instance Variable STM TMVar where
 instance Locked STM TMVar where
 
 
+-- | @varState v m@ runs @m@ over the value stored in @v@, storing the resulting
+-- value in @v@.
 varState :: (Variable m v) => v s -> State  s   a -> m a
 varState v = modifyVar v . runState
 
+-- | A strict version of `varState`.
 varState' :: (Variable m v) => v s -> State  s   a -> m a
 varState' v = modifyVar' v . runState
 
+-- | @varStateT v m@ runs @m@ over the value stored in @v@, storing the
+-- resulting value in @v@.
+--
+-- Note that @m@ may perform arbitrary computations in the base monad of
+-- the variable. Therefore, the variable has the constraint @Locked m v@
+-- which should only be defined for variables with the proper locking
+-- semantics.
+--
 varStateT :: (Locked m v) => v s -> StateT s m a -> m a
 varStateT v = atomicVar v . runStateT
 
 
+-- | An infix version of `varState`.
 (@:) :: (Variable m v) => v s -> State s a -> m a
 (@:) = varState
 infix 2 @:
 
+-- | An infix version of `varState'`.
 (@!) :: (Variable m v) => v s -> State s a -> m a
 (@!) = varState'
 infix 2 @!
 
+-- | An infix version of `varStateT`.
 (@@) :: (Locked m v) => v s -> StateT s m a -> m a
 (@@) = varStateT
 infix 2 @@
